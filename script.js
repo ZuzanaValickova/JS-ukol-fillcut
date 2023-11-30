1668b3749ffccc44f8b3f490dadad0d60c4f0650
Zuzana Valickova <zuzana.valickova@icloud.com>
2023-11-30 21:43:20 +0000

    přidán index.html a script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+/*
+
+Napište funkci fillcut, která jako svůj první parametr str očekává řetězec a jako druhý parametr len kladné celé číslo. Úkolem funkce je oříznout nebo prodloužit zadaný řetězec tak, aby měl délku přesně len.
+Pokud je vstupní řetězec delší než len, tak funkce odřízne jeho konec a vrátí výsledek.
+Pokud je vstupní řetězec kratší než len, tak jej doplní od začátku znakem tečky a vrátí výsledek.
+Pokud je vstupní řetězec dlouhý přesně len, funkce jej vrátí beze změny.
+Příklad použití:
+
+document.body.innerHTML += fillcut('petr', 8) + '<br>'; // vypíše „....petr“
+document.body.innerHTML += fillcut('petr', 3) + '<br>'; // vypíše „pet“
+document.body.innerHTML += fillcut('petr', 4) + '<br>'; // vypíše „petr“
+*/
+
+const fillcut = (str, len) => {
+    if (!Number.isInteger(len) || len <= 0) {
+        return str
+    }
+
+    if (str.length > len) {
+        return str.slice(0, len)
+    }
+
+    while (str.length < len) {
+        str = "." + str 
+    }
+
+    return str;
+}
+
+document.body.innerHTML += fillcut('petr', 8) + '<br>'
+document.body.innerHTML += fillcut('petr', 3) + '<br>'
+document.body.innerHTML += fillcut('petr', 4) + '<br>'
